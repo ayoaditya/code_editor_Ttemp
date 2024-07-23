7a632c230ecf10f0d3dc2b041662fd4d976795ed
ayoaditya <adityakhatana9@gmail.com>
2024-07-23 11:15:34 +0530

    expresss

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
-const express = require("express");
+import express from "express";
+
 const app = express();
-const port = process.env.PORT || 5000;
+const port = 5000;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
